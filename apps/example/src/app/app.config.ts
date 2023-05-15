eb18bff250a8e8aa90f65e89b17973f0118ca55e
Robin Goetz <tug29225@temple.edu>
2023-05-15 16:16:32 -0400

    chore: upgrade nx and angular to v16

---
+++ apps/example/src/app/app.config.ts
@@ -0,0 +1,6 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes)],
+};
--- apps/example/src/main.ts
+++ apps/example/src/main.ts
@@ -1,9 +1,5 @@
-import {bootstrapApplication} from "@angular/platform-browser";
-import {AppComponent} from "./app/app.component";
-import {provideRouter} from "@angular/router";
-import {routes} from "./app/app.routes";
+import { appConfig } from './app/app.config';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
 
-
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)]
-})
+bootstrapApplication(AppComponent, appConfig);
